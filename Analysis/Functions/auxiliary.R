f17a56b098d0e4fdde9ce5e398e13e8a1dcf1e70
Nils Eling <nils.eling@gmx.de>
2018-07-18 22:37:50 +0100

    New functions to work with sce objects

--- Analysis/Functions/auxiliary.R
+++ Analysis/Functions/auxiliary.R
@@ -1,116 +1,149 @@
-#############################################
-#### Script to store auxiliary functions ####
-#############################################
+###############################################
+#### Script to collect auxiliary functions ####
+###############################################
 
-# load data by tissue
-loadData <- function(file_vec){
-  
-  # load normalized data
-  normdata <- sapply(file_vec, readRDS)
-  
-  # add patients to column names
-  ps <- sapply( strsplit(file_vec, "/"), grep, pattern="Patient", value=TRUE)
-  for(i in 1:length(normdata)){
-    colnames(normdata[[i]]) <- paste(ps[i], colnames(normdata[[i]]), sep="_")
+# Load libraries needed in this script
+library(scater)
+library(scran)
+library(dynamicTreeCut)
+library(princurve)
+library(dbscan)
+library(edgeR)
+library(destiny)
+
+#### Split Single cell experiment
+split.sce <- function(sce, groups, colData.name = "SubCluster"){
+  # List to collect individual single cell experiments
+  list.out <- list()
+  for(i in groups){
+    cur_sce <- sce[,as.character(colData(sce)[[colData.name]]) == as.character(i)]
+    cur_sce <- normalize(cur_sce)
+    list.out[[i]] <- cur_sce
   }
-  
-  if(length(file_vec)>1){
-    # reduce to genes present in all data sets
-    genes <- lapply(normdata, row.names)
-    genes <- table(unlist(genes))
-    genes <- names(genes)[genes>=length(normdata)]
-    
-    # reduce to genes expressed in at least one sample
-    normdata_sub <- sapply(normdata, function(x, g) as.matrix(x[g, ]), g=genes)
-    genes_sub <- genes[rowSums(do.call("cbind", normdata_sub))>0]
-    normdata_sub <- sapply(normdata, function(x, g) as.matrix(x[g, ]), g=genes_sub)
-    
-    return(normdata_sub)
-  }else{
-    return(normdata)
+  names(list.out) <- groups
+  list.out
+}
+
+#### HVG
+# Calculate highly variable genes
+# sce: one or multiple single cell experiment objects in form of a list
+HVG <- function(sce, numberGenes = 1000){
+  # One single cell expereiment object
+  if(typeof(sce) == "S4"){
+    HVG <- trendVar(sce, use.spikes = FALSE)
+    HVG.1 <- decomposeVar(sce, HVG)
+    HVG.1 <- HVG.1[order(HVG.1$bio, decreasing = TRUE),]
+    rownames(HVG.1)[1:numberGenes]
+  }
+  # Multiple single cell experiment objects
+  else if(typeof(sce) == "list") {
+    lapply(sce, function(n){
+      HVG <- trendVar(n, use.spikes = FALSE)
+      HVG.1 <- decomposeVar(n, HVG)
+      HVG.1 <- HVG.1[order(HVG.1$bio, decreasing = TRUE),]
+      rownames(HVG.1)[1:numberGenes]
+    })
+  }
+  else{
+    print("First argument must either be a single cell experiment object \n
+          or a list")
   }
 }
 
-# filter contaminating cells
-filterData <- function(normdata_list, cd45=TRUE, vim=TRUE){
-  # prep
-  select <- sapply(normdata_list, function(x) rep(TRUE, ncol(x)))
-  
-  # helper
-  filterhelper <- function(geneid, normdata, select){
-    if(geneid%in%row.names(normdata)){
-      select <- select & (!normdata[geneid, ]>0)
-    }else{
-      print("gene not measured")
+#### Clustering
+# Perform clustering using dynamic tree cut
+DTC <- function(sce, HVG.genes, minClusterSize = 10, deepSplit = 0){
+  if(typeof(sce) == "S4"){
+    dist.all <- as.dist(sqrt((1 - cor(as.matrix(logcounts(sce)[HVG.genes,]), 
+                                      method = "spearman"))/2))
+    
+    dendro <- hclust(dist.all, method = "ward.D2")
+    
+    ct <- as.character(cutreeDynamic(dendro = dendro, distM = as.matrix(dist.all), 
+                        minClusterSize = minClusterSize, deepSplit = deepSplit))
+  }
+  else {
+    out <- list()
+    for(i in 1:length(sce)){
+      dist.all <- as.dist(sqrt((1 - cor(as.matrix(logcounts(sce[[i]])[HVG.genes[[i]],]), 
+                                        method = "spearman"))/2))
+      
+      dendro <- hclust(dist.all, method = "ward.D2")
+      
+      cur_clusters <- paste(names(sce)[i], as.character(cutreeDynamic(dendro = dendro, 
+                                                      distM = as.matrix(dist.all), 
+                minClusterSize = minClusterSize, deepSplit = deepSplit)), sep = "_")
+      names(cur_clusters) <- colData(sce[[i]])$Barcode
+      
+      out[[names(sce)[i]]] <- cur_clusters
     }
-    return(select)
+    names(out) <- names(sce)
+    out
   }
-  
-  # filter immune cells
-  if(cd45) {
-    select <- sapply(1:length(normdata_list), function(i) 
-      filterhelper("ENSG00000081237", normdata_list[[i]], select[[i]]))
-  }
-  
-  if(vim) {
-    select <- sapply(1:length(normdata_list), function(i) 
-      filterhelper("ENSG00000026025", normdata_list[[i]], select[[i]]))
-  }
-  normdata_list <- sapply(1:length(normdata_list), function(i) 
-    return(normdata_list[[i]][,select[[i]]]))
-  
-  return(normdata_list)
 }
 
-# highly variable genes 
-getHVGs <- function(normdata_list){
+#### Find specifc marker genes
+marker.detection <- function(sce, clusters){
+  # User scran function findMarkers to perform differential expression
+  cur_markers <- findMarkers(sce, clusters)
   
-  # remove lowly expressed genes
-  genes <- lapply(normdata_list, function(x) row.names(x)[rowMeans(x)>0.1])
-  genes <- table(unlist(genes))
-  genes <- names(genes)[genes==length(normdata_list)]
-  normdata_list <- lapply(normdata_list, function(x, g) x[g,], g=genes)
+  # Collect group specific markers
+  markers.spec <- lapply(cur_markers, function(n){
+    if(!is.na(n$Top[1])){
+    cur_n <- n[n$FDR < 0.1 & apply(n[,3:ncol(n)], 1, function(x){sum(x > 0)}) == ncol(n) - 2,]
+      if(nrow(cur_n) > 0){
+        cur_n$GeneName <- rowData(sce)$Symbol[match(rownames(cur_n), rowData(sce)$ID)]
+      }
+    }
+    else{
+      cur_n <- NULL
+    }
+    cur_n
+  })
   
-  # find highly variable genes, single data set
-  hvgHelper <- function(data){
-    # fit variance mean relationship
-    logdata <- log2(data + 1)
-    varfit <- trendVar(logdata)
-    decomp <- decomposeVar(logdata, varfit) 
-    
-    # plot fit
-    #plot(varfit$mean, varfit$var)
-    #curve(varfit$trend(x), col="red", lwd=2, add=TRUE)
-    
-    return( decomp )
-  }
-  par(mfrow=c(1,length(normdata_list)))
-  HVG_list <- lapply(normdata_list, hvgHelper)
-  
-  # combine if more than one sample
-  if(length(HVG_list)>1){
-    # unpack arguments for combineVar
-    HVG.df <- do.call(combineVar, HVG_list)
-  }else{
-    HVG.df <- HVG_list[[1]]
-  }
-  
-  # get top 1000 most variable genes
-  HVG.df <- HVG.df[order(HVG.df$bio, decreasing=TRUE), ]
-  HVG <- rownames(HVG.df)[1:1000]
-  return(HVG)
 }
 
-# batch correct by tissue
-batchCorrect <- function(normdata_list, HVG){
-  # batch correction based on top highly variable genes
-  normdata_list_hvg <- sapply(normdata_list, function(x, hvg) log2(x[hvg, ] +1), hvg=HVG)
+#### Compute pseudotime with destiny
+diffusionPT <- function(sce, HVG, clusters, col_vector,
+               exclude = NULL){
+  if(!is.null(exclude)){
+    dm <- DiffusionMap(t(as.matrix(logcounts(sce)[HVG,!exclude])), k = 20)
+    
+    plot(dm, col = col_vector[clusters[!exclude]], 
+         pch = 16, type = "p")
+    
+    dpt <- DPT(dm = dm)
+    
+    dpt$DPT1
+  }
+  else{
+    dm <- DiffusionMap(t(as.matrix(logcounts(sce)[HVG,])), k = 20)
+    
+    plot(dm, col = col_vector[clusters], 
+         pch = 16, type = "p")
+    
+    dpt <- DPT(dm = dm)
+    
+    dpt$DPT1
+  }
+}
+
+#### Batch correction
+batch.correction <- function(sce, number.HVG = 1000){
+  # Calculate highly variable genes and merge
+  HVG.genes <- lapply(sce, function(n){
+    HVG <- trendVar(n, use.spikes = FALSE)
+    decomposeVar(n, HVG)
+  })
   
-  # unpack arguments for mnnCorrect
-  funcText <- paste0("mnnCorrect(", 
-                     paste0("normdata_list_hvg[[", 1:length(normdata_list_hvg), "]]", collapse=", "), 
+  HVG.df <- do.call("combineVar", HVG.genes)
+  HVG.df <- HVG.df[order(HVG.df$bio, decreasing = TRUE),]
+  genes <- rownames(HVG.df)[1:number.HVG]
+  
+  # Batch correction
+  func <- paste0("mnnCorrect(", 
+                     paste0("as.matrix(logcounts(sce[[", 1:length(sce), "]])[genes,])", collapse=", "), 
                      ", cos.norm.in=TRUE, cos.norm.out=TRUE, sigma=0.1)")
-  bcdata <- eval( parse(text=funcText) )
-  
-  return(bcdata)
+  corrected <- eval( parse(text=func) )
+  do.call("cbind", corrected$corrected)
 }
